8a6b4c64961b4b3fb399b57c50d20a2a84d64e42
guillaume.larue <guillaume.larue@epita.fr>
2023-01-14 18:25:26 +0100

    [FIX] fic color test

--- test_given.sh
+++ test_given.sh
@@ -5,21 +5,22 @@ failed=0
 
 for i in {2..6}
 do
-echo -e "\e[1;34m------------Test $i*$i------------\e[0m"
+  #\033[0;31m
+echo -e "\033[1;34m------------Test $i*$i------------\033[0m"
 for j in {1..9}
 do
     src="test/given/input/s$i-0$j.txt"
     ref="test/given/ref/s$i-0$j-ref.txt"
     ./a.out $src comp.txt
     if diff -b comp.txt $ref &>/dev/null; then
-        echo -e "\t\e[1;32m$j : Test passed\e[0m"
+        echo -e "\t\033[1;32m$j : Test passed\033[0m"
         passed=$((passed+1))
     else
-        echo -e "\t\e[1;31m$j : Test failed\e[0m"
+        echo -e "\t\033[1;31m$j : Test failed\033[0m"
         failed=$((failed+1))
     fi
 done
 done 
-echo -e "\n\e[1;34m------------Summary-------------\e[0m"
-echo -e "\e[1;32m\tPassed $passed\e[0m"
-echo -e "\e[1;31m\tFailed $failed\e[0m"
+echo -e "\n\033[1;34m------------Summary-------------\033[0m"
+echo -e "\033[1;32m\tPassed $passed\033[0m"
+echo -e "\033[1;31m\tFailed $failed\033[0m"
