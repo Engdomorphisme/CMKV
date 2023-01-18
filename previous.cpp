4619a954a20007f31d569c6a7121b44205e3a905
Enguerrand de Gentile Duquesne <enguerrand.de-gentile-duquesne@epita.fr>
2023-01-18 14:28:20 +0100

    a

--- previous.cpp
+++ previous.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <experimental/random>
 #include <fstream>
 #include <iostream>
 #include <vector>
@@ -170,56 +171,98 @@ Board swapColumns(Board board, int i, int j)
     return board;
 }
 
-void swapTiles(Board board, int i, int j)
-{
-    int n = std::sqrt(board.size());
-    auto begin = board.begin();
-    std::iter_swap(begin + i, begin + j);
-}
-
 Board solve(Board &board)
 {
-    // Use a Metropolis algorithm to solve the puzzle
-    int n = std::sqrt(board.size());
+    // TODO: Solve the board and return the solution board (or nullptr if no
+    // solution exists
+
+    // Use a Metropolis-Hastings algorithm to solve the board
+    // https://en.wikipedia.org/wiki/Metropolis%E2%80%93Hastings_algorithm
+
+    // 1. Generate a random solution
+    // 2. Evaluate the solution
+    // 3. Generate a random neighbor
+    // 4. Evaluate the neighbor
+    // 5. If the neighbor is better, accept it
+    // 6. If the neighbor is worse, accept it with probability e^(-delta /
+    // T)
+    // 7. Repeat until the solution is good enough
+
+    int size = board.size();
+    int n = std::sqrt(size);
     int score = evaluate(board);
     int bestScore = score;
     Board bestBoard = board;
     int iterations = 0;
+    int maxIterations = 1000000;
+    srand(time(nullptr));
 
     while (score > 0)
     {
-        int i = rand() % n;
-        int j = rand() % n;
+        int i = std::experimental::randint(0, size - 1);
+        int j = std::experimental::randint(0, size - 1);
+        if (i == j)
+        {
+            j = (j + 1) % size;
+        }
 
-        swapTiles(board, i, j);
+        Tiles temp = board[i];
+        board[i] = board[j];
+        board[j] = temp;
+
+        // Evaluate the neighbor
         score = evaluate(board);
-
+        // If the neighbor is better, accept it
         if (score < bestScore)
         {
             bestScore = score;
             bestBoard = board;
         }
+        // If the neighbor is worse, accept it with probability e^(-delta /
+        // T)
         else
         {
-            swapTiles(board, i, j);
+            double T = 1.0 / (1.0 + iterations);
+            double delta = score - bestScore;
+            double p = exp(-delta / T);
+            if (p < (double)rand() / RAND_MAX)
+            {
+                bestScore = score;
+                bestBoard = board;
+            }
+            else // Reject the neighbor
+            {
+                board = bestBoard;
+            }
         }
+
         iterations += 1;
     }
-    std::cout << "Iterations: " << iterations << std::endl;
     return bestBoard;
 }
 
 int main(int argc, char *argv[])
 {
-    std::ifstream in(argv[1]);
-    std::ofstream out(argv[2]);
-    int n = argv[1][12] - '0';
-    Board board(n * n, std::vector<int>(4, 0));
+    if (argc == 3 or argc == 4)
+    {
+        std::ifstream in(argv[1]);
+        std::ofstream out(argv[2]);
 
-    board = readBoard(in);
-    auto solution = solve(board);
+        auto board = readBoard(in);
+        auto solution = solve(board);
 
-    writeBoard(out, solution);
+        writeBoard(out, solution);
 
-    return 0;
+        // To check the solution
+        if (argc == 4)
+        {
+            printBoard(solution);
+            std::ifstream solutionIn(argv[3]);
+            auto solutionBoard = readBoard(solutionIn);
+            printBoard(solutionBoard);
+        }
+
+        return 0;
+    }
+    return 1;
 }
